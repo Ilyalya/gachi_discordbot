d45ef12525a8e25d230b3a970f49f65eed728bde
ilya <ilya.lebedev.98@mail.ru>
2021-11-10 11:24:23 +0300

    Пожилые изменения

--- billy/botrun.py
+++ billy/botrun.py
@@ -3,49 +3,49 @@ from discord.ext import commands
 import youtube_dl
 import asyncio
 
-# youtube_dl.utils.bug_reports_message = lambda: ''
-#
-# ytdl_format_options = {
-#     'format': 'bestaudio/best',
-#     'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-#     'restrictfilenames': True,
-#     'noplaylist': True,
-#     'nocheckcertificate': True,
-#     'ignoreerrors': False,
-#     'logtostderr': False,
-#     'quiet': True,
-#     'no_warnings': True,
-#     'default_search': 'auto',
-#     'source_address': '0.0.0.0'  # привязать к ipv4, поскольку адреса ipv6 иногда вызывают проблемы
-# }
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'  # привязать к ipv4, поскольку адреса ipv6 иногда вызывают проблемы
+}
 
 ffmpeg_options = {
     'options': '-vn'
 }
 
-# ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
 
-# class YTDLSource(discord.PCMVolumeTransformer):
-#     def __init__(self, source, *, data, volume=0.5):
-#         super().__init__(source, volume)
-#
-#         self.data = data
-#
-#         self.title = data.get('title')
-#         self.url = data.get('url')
-#
-#     @classmethod
-#     async def from_url(cls, url, *, loop=None, stream=False):
-#         loop = loop or asyncio.get_event_loop()
-#         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-#
-#         if 'entries' in data:
-#             # берем первый элемент плейлиста
-#             data = data['entries'][0]
-#
-#         filename = data['url'] if stream else ytdl.prepare_filename(data)
-#         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # берем первый элемент плейлиста
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
 class Music(commands.Cog):
@@ -70,25 +70,25 @@ class Music(commands.Cog):
 
         await ctx.send()
 
-    # @commands.command()
-    # async def yt(self, ctx, *, url):
-    #     """Вопроизведение по url адресу"""
-    #
-    #     async with ctx.typing():
-    #         player = await YTDLSource.from_url(url, loop=self.bot.loop)
-    #         ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-    #
-    #     await ctx.send('Now playing: {}'.format(player.title))
-    #
-    # @commands.command()
-    # async def stream(self, ctx, *, url):
-    #     """Потоки с URL-адреса (такие же, как у yt, но без предварительной загрузки)"""
-    #
-    #     async with ctx.typing():
-    #         player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-    #         ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-    #
-    #     await ctx.send('Now playing: {}'.format(player.title))
+    @commands.command()
+    async def yt(self, ctx, *, url):
+        """Вопроизведение по url адресу"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop)
+            ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+        await ctx.send('Now playing: {}'.format(player.title))
+
+    @commands.command()
+    async def stream(self, ctx, *, url):
+        """Потоки с URL-адреса (такие же, как у yt, но без предварительной загрузки)"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+            ctx.voice_client.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
+
+        await ctx.send('Now playing: {}'.format(player.title))
 
     @commands.command()
     async def volume(self, ctx, volume: int):
@@ -107,8 +107,8 @@ class Music(commands.Cog):
         await ctx.voice_client.disconnect()
 
     @play.before_invoke
-    # @yt.before_invoke
-    # @stream.before_invoke
+    @yt.before_invoke
+    @stream.before_invoke
     async def ensure_voice(self, ctx):
         if ctx.voice_client is None:
             if ctx.author.voice:
@@ -124,17 +124,35 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"))
 
 
 @bot.event
-async def on_ready():
+async def on_ready(message):
     print('{0} go to the gym ({0.id})'.format(bot.user))
     print('------')
 
 
-# @bot.event
-# async def on_message(ctx):
-#     if 'cum' in ctx.content.lower():
-#         source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio('/Users/lebedevila/PycharmProjects/gachi_discordbot/sounds/Swallow my cum.mp3'))
-#         ctx.voice_client.play(source, after=lambda e: print('Player error: %s' % e) if e else None)
-#         await ctx.send()
+"""вот пример как я хочу совместить"""
+@bot.event
+async def on_message(ctx):
+    if 'cum' in ctx.content.lower():
+        query = '/Users/lebedevila/PycharmProjects/gachi_discordbot/sounds/Swallow my cum.mp3'
+        source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(query))
+        ctx.voice_client.play(source)
+
+        await ctx.send()
+
+    await bot.process_commands(ctx)
+
+"""вот это работает"""
+@bot.event
+async def on_message(message):
+    if 'дела' in message.content.lower():
+        await message.channel.send("♂️Ass we can♂️")
+    elif 'привет' in message.content.lower():
+        await message.channel.send("♂️My fellow brothers! "
+                                   "I, Billy Herrington, stand here today, "
+                                   "humbled by the task before us, mindful "
+                                   "of the sacrifices borne by our Nico Nico ancestors♂️")
+
+    await bot.process_commands(message)
 
 bot.add_cog(Music(bot))
-bot.run('ODg4NDc4MjQ0Njc5MDYxNTQ0.YUTR2A.7ceFT44hDBh1r42Yb3DZcYhZuiY')
+bot.run('token')
